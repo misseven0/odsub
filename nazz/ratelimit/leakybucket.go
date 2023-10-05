53dd2cf0e30084ecdbf65af33dcbe8e11a6f2298
misseven0 <misseven@protonmail.com>
2023-10-05 21:16:03 +0800

    bkucket

--- nazz/ratelimit/leakybucket.go
+++ nazz/ratelimit/leakybucket.go
@@ -2,7 +2,7 @@
 // https://github.com/q191201771/naza
 //
 // Use of this source code is governed by a MIT-style license
-// that can be found in the License file.
+// that can be found in the License file.	w
 //
 // Author: Chef (191201771@qq.com)
 
