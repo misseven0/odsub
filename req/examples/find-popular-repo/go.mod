517851850ff11230d3157227aa5ddd791b30aaaa
misseven0 <misseven@protonmail.com>
2024-02-13 04:34:47 +0800

    ex

---
+++ req/examples/find-popular-repo/go.mod
@@ -0,0 +1,29 @@
+module find-popular-repo
+
+go 1.18
+
+replace github.com/imroc/req/v3 => ../../
+
+require github.com/imroc/req/v3 v3.0.0
+
+require (
+	github.com/cheekybits/genny v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/marten-seemann/qpack v0.2.1 // indirect
+	github.com/marten-seemann/qtls-go1-16 v0.1.5 // indirect
+	github.com/marten-seemann/qtls-go1-17 v0.1.2 // indirect
+	github.com/marten-seemann/qtls-go1-18 v0.1.3 // indirect
+	github.com/marten-seemann/qtls-go1-19 v0.1.1 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
---
+++ req/examples/find-popular-repo/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/imroc/req/v3"
+)
+
+// Change the name if you want 2
+var username = "imroc"
+
+func main() {
+	repo, star, err := findTheMostPopularRepo(username)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("The most popular repo of %s is %s, which have %d stars\n", username, repo, star)
+}
+
+func init() {
+	req.EnableDebugLog().
+		EnableTraceAll().
+		EnableDumpEachRequest().
+		SetCommonErrorResult(&ErrorMessage{}).
+		OnAfterResponse(func(client *req.Client, resp *req.Response) error {
+			if resp.Err != nil {
+				return nil
+			}
+			if errMsg, ok := resp.ErrorResult().(*ErrorMessage); ok {
+				resp.Err = errMsg
+				return nil
+			}
+			if !resp.IsSuccessState() {
+				resp.Err = fmt.Errorf("bad status: %s\nraw content:\n%s", resp.Status, resp.Dump())
+			}
+			return nil
+		})
+}
+
+type Repo struct {
+	Name string `json:"name"`
+	Star int    `json:"stargazers_count"`
+}
+type ErrorMessage struct {
+	Message string `json:"message"`
+}
+
+func (msg *ErrorMessage) Error() string {
+	return fmt.Sprintf("API Error: %s", msg.Message)
+}
+
+func findTheMostPopularRepo(username string) (repo string, star int, err error) {
+	var popularRepo Repo
+	var resp *req.Response
+
+	for page := 1; ; page++ {
+		repos := []*Repo{}
+		resp, err = req.SetHeader("Accept", "application/vnd.github.v3+json").
+			SetQueryParams(map[string]string{
+				"type":      "owner",
+				"page":      strconv.Itoa(page),
+				"per_page":  "100",
+				"sort":      "updated",
+				"direction": "desc",
+			}).
+			SetPathParam("username", username).
+			SetSuccessResult(&repos).
+			Get("https://api.github.com/users/{username}/repos")
+
+		fmt.Println("TraceInfo:")
+		fmt.Println("----------")
+		fmt.Println(resp.TraceInfo())
+		fmt.Println()
+
+		if err != nil {
+			return
+		}
+
+		if !resp.IsSuccessState() { //  HTTP status `code >= 200 and <= 299` is considered as success by default
+			return
+		}
+		for _, repo := range repos {
+			if repo.Star >= popularRepo.Star {
+				popularRepo = *repo
+			}
+		}
+		if len(repo) == 100 { // Try Next page
+			continue
+		}
+		// All repos have been traversed, return the final result
+		repo = popularRepo.Name
+		star = popularRepo.Star
+		return
+	}
+}
