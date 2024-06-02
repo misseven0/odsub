7cc9c2be1c09e71d14bdd20df20573293c4dc730
misseven0 <misseven@protonmail.com>
2024-06-02 16:24:57 +0800

    add config

---
+++ vray/reverse/config.go
@@ -0,0 +1,17 @@
+// +build !confonly
+
+package reverse
+
+import (
+	"crypto/rand"
+	"io"
+
+	"v2ray.com/core/common/dice"
+)
+
+//config
+func (c *Control) FillInRandom() {
+	randomLength := dice.Roll(64)
+	c.Random = make([]byte, randomLength)
+	io.ReadFull(rand.Reader, c.Random)
+}
